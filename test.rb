94a5ba55453262c9327742f99c0960ec6f1fa518
Kriti Sen Sharma <ksen@paradigm4.com>
2016-07-05 10:03:52 -0400

    Update test.rb

--- test.rb
+++ test.rb
@@ -7,3 +7,14 @@ puts profile.current_companies
 puts profile.past_companies
 puts profile.education
 puts profile.industry
+
+profiles = %q(
+  https://www.linkedin.com/in/bipuldas
+  https://www.linkedin.com/in/asrivastava
+  https://www.linkedin.com/in/atul-narayan-3097334
+  https://www.linkedin.com/in/pranav-prabhash-686a801b
+  https://www.linkedin.com/in/vivekmalewar
+  https://www.linkedin.com/in/kumar-abhishek-singh-b9341516
+  https://www.linkedin.com/in/pramod-doguparty-5146a54
+  https://www.linkedin.com/in/srikanta-mukherjee-44531624
+            )
